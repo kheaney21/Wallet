c11a539051f2ea01b67feb8b5ef5daf10532cab6
Kelly Heaney <31257783+kheaney21@users.noreply.github.com>
2018-10-24 23:47:46 -0700

    Add files via upload

--- Wallet.py
+++ Wallet.py
@@ -1,11 +1,7 @@
 import sys
 import random
-
-#import pycrypto as pyc
-#from Crypto.PublicKey import RSA
-#from Crypto import Random
-
-#rgen = Random.new().read
+import pycrypto
+from Crypto.Hash import SHA256
 
 class Wallet:
 	'Class for wallet object'
@@ -22,15 +18,18 @@ class Wallet:
 		return balance
 		
 	def getPublicKey():
+		return keys[2]
+		
+	def getMod():
 		return keys[1]
 	
-	#assume recipient is registered with the bank
+	#assume recipient is registered with the bank (checked in driver)
 	def payment(self, amount):
 		#check for sufficient funds
 		if(sufFunds):
 			self.balance = balance - amount
 	
-	#assume target is registered with the bank
+	#assume target is registered with the bank (checked in driver)
 	def addFunds(self, amount):
 		self.balance = balance + amount
 		
@@ -42,26 +41,117 @@ class Wallet:
 		else:
 			return True
 		
-	#generate a number and check if prime via
-	def genPrime(self, n):
-		
-		#loop?
-		
-		#generate pseudorandom number
+	def genPrime(self, n, k):
 		x = random.randint(3, n, 2)
+		while not (isPrime(x, k)):
+			x = random.randint(3, n, 2)
+		return x
+			
+	#check if prime via miller-rabin test
+	def isPrime(self, n, k):
+		
+		#calculate d
+		n = temp
+		s = 0
+		while temp % 2 == 0:
+			temp = temp/2
+			s = s + 1
+			
+		d = n/(2**s)
+		
+		count = 0
+		while count < k
+			#update counter
+			count = count + 1
+			#generate pseudorandom number
+			a = random.randint(2, n - 1)
+			x = a**d % n
+			if x = 1 or x = n - 1:
+				continue
+			for i in range (s-1):
+				x = (x**2) % n
+				if x = 1:
+					return False
+				if x = n - 1:
+					continue
+				
+			return False
+		return True
+			
+		
+	def genCoprime(p, q):
+		e = random.randint(3, n, 2)
+		while not (e % p - 1 != 0 and e % p - 1 != 0):
+			e = random.randint(3, n, 2)	
+		return e
 		
 	def genKeys():
 		#maximum number
-		n = 10,000
-		x = genPrime(n)
+		max = 10,000
+		#number of iterations
+		k = 10,000
+		p = genPrime(max, k)
+		q = genPrime(max, k)
+				
+		#modulus
+		n = p * q
+		
+		#public exponent
+		e = genCoprime(p, q)
+		
+		#integer k
+		k = random.randint(2, n)
+		
+		#private exponent
+		d = (1 + k(p - 1)(q - 1))/e
 		
 		#generate private key
-		#privKey = 
+		privKey = d
 		#generate public key
-		#pubKey = 
+		pubKey = e
+		#modulus
+		mod = n
 		
-		#keys = (pubKey, privKey)
-		#return keys
+		keys = (mod, pubKey, privKey)
+		return keys
+		
+	def getHash(self, message):
+		#get hash
+		hash = SHA256.new(message)
+		return hash
+		
+	#generates signature for a message	
+	def sign(self, hashValue):
+		#encrypt
+		d = self.keys[3]
+		n = self.keys[1]
+		signature = hashValue**(d % n)
+		return signature
+		
+	def sendRequest(self, message):
+		hashValue1 = self.getHash(message)
+		#modulus
+		n = self.keys[1]
+		#public key
+		e = self.keys[1]
+		signature = self.sign(hashValue1)
+		request = (n, e, signature, hashValue1)
+		return request
+		
+	#verifies that signature matches hash value	
+	def verifySig(self, request):
+		#decrypt
+		n = request[1]
+		e = request[2]
+		signature = request[3]
+		hashValue1 = request[4]
+		
+		hashValue2 = signature**(e % n)
+		if hashValue1 == hashValue2:
+			return True
+		return False
+		
+		
 		
 		
 		
--- walletDriver.py
+++ walletDriver.py
@@ -1,60 +1,70 @@
 import sys
 
-bank = Bank()
-accts = bank.getAccts() #dictionary of accounts
+class WalletDriver:
+	'Driver for wallet project'
+	def __init__(self):
+		bank = Bank() #problem here
+		accts = bank.getAccts() #dictionary of accounts
 
-def getInput():
-	option1 = raw_input("New or returning user? (new/return)" )
-	if(option1.lower() == "new"):
-		newWallet()
-	elif(option1.lower() == "return"):
-		returnUser()
-	else:
-		print("Invalid input")
+	def getInput(self):
+		option1 = raw_input("New or returning user? (new/return)" )
+		if(option1.lower() == "new"):
+			self.newWallet()
+		elif(option1.lower() == "return"):
+			self.returnUser()
+		else:
+			print("Invalid input")
 		
 	
-def newWallet():
-	addr = raw_input("Enter email address")
-	#check if address is taken
-	if(accts.has_key(addr)):
-		return "Email address is already in use"
-	pswd = raw_input("Enter password")
-	bank.newAcct(addr, pswd)
-	accts = bank.getAccts()
-	
-	
-def returnUser():
-	addr = raw_input("Enter email address")
-	if(login(addr) == True):
-		toContinue = 1
-		while toContinue == 1:
-			option2 = input("1 - Check balance \n 2 - Make a Payment")
-			if(option2 == 1):
-				return accts.getBalance()
-			elif(option2 == 2):
-				recipient = raw_input("Enter email address of recipient")
-				#check if valid recipient
-				if(accts.has_key(recipient)):
-					amount = raw_input("Enter amount in USD")
-					if(accts[addr].sufFunds(amount)):
-						accts[addr].payment(amount)
-						accts[recipient].addFunds(amount)
-				else:
-					print("Invalid recipient")
-				
-			else:
-				print("Invalid Input")
-			toContinue = 0
-			toContinue = raw_input("Enter 1 to continue")
-				
-def login(self, addr):
-	
-	if not bank.getAccts().has_key(addr):
-		print("Invalid address")
-	else:
+	def newWallet(self):
+		addr = raw_input("Enter email address")
+		#check if address is taken
+		if(accts.has_key(addr)):
+			return "Email address is already in use"
 		pswd = raw_input("Enter password")
-		if(pswd == accts[addr].getPassword()):
-			return True	
+		bank.newAcct(addr, pswd)
+		accts = bank.getAccts()
+	
+	
+	def returnUser(self):
+		addr = raw_input("Enter email address")
+		if(login(addr) == True):
+			toContinue = 1
+			while toContinue == 1:
+				user = accts[addr]
+				option2 = input("1 - Check balance \n 2 - Make a Payment")
+				if(option2 == 1):
+					return user.getBalance()
+				elif(option2 == 2):
+					recipient = raw_input("Enter email address of recipient")
+					#check if valid recipient
+					if(accts.has_key(recipient)):
+						amount = raw_input("Enter amount in USD")
+						if(user.sufFunds(amount)):
+							signature = user.sign(amount)
+							request = user.sendRequest(signature)
+							if(accts[recipient].verifySig(request)):
+								user.payment(amount)
+								accts[recipient].addFunds(amount)
+							else:
+								print("Signature verification failed")
+					else:
+						print("Invalid recipient")	
+				else:
+					print("Invalid Input")
+				toContinue = 0
+				toContinue = raw_input("Enter 1 to continue")
+				
+	def login(self, addr):
+		if not bank.getAccts().has_key(addr):
+			print("Invalid address")
 		else:
-			print("Invalid password")
-			return False
+			pswd = raw_input("Enter password")
+			if(pswd == accts[addr].getPassword()):
+				return True	
+			else:
+				print("Invalid password")
+				return False
+		
+driver = WalletDriver()
+driver.getInput()
